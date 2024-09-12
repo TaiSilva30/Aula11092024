ec53194aa4fbb783519ce09610590a56927ea27d
Tai Silva30 <tatihsilva16@gmail.com>
2024-09-11 21:58:36 -0300

    Desafio 5

---
+++ Desafio/Desafio5.py
@@ -0,0 +1,8 @@
+coordenadas = (1, 2, 3)
+print(coordenadas)
+
+valoratt_y = int(input("Digite um novo valor para y: "))
+
+coordenadas = (coordenadas[0], valoratt_y, coordenadas[2])
+
+print(coordenadas)
